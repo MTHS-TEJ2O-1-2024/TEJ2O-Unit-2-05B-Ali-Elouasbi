bc09a75310298db94fac2aa2c7b40b013f88e4d8
Ali-Elouasbi <ali.elouasbi@mths.ca>
2024-09-27 10:40:18 -0400

    Done

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Ali Elouasbi
+ * Created on: Sep 2024
+
+* This program says the temperature
 */
 
-basic.showString('Hello, World!')
+// our variable for a random number
+let temperature = 0
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    temperature = input.temperature()
+    basic.showString('The temperature is:')
+
+    // output answer
+    basic.clearScreen()
+    basic.showString('294 k.')
+})
